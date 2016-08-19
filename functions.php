3c1dcea46be480d4af2e9990d4f60ae29af56380
Jerry <938334866@qq.com>
2016-08-19 19:25:02 +0800

    优化数据库连接文件，密码加密

--- functions.php
+++ functions.php
@@ -7,15 +7,21 @@
  * Time: 11:54
  */
 
-$db_hostname = "localhost";
-$db_database = "Nest";
-$db_username = "root";
-$db_password = "";
-$appName = "Jerry's Nest";
+//密码加密使用PASSWORD函数进行字符串16进制，或者AES——DESRCPT二进制加密
 
-$db_server = mysqli_connect($db_hostname, $db_username, $db_password);
+define(DB_HOSTNAME,"localhost");
+define(DB_DATABASE, "Nest");
+define(DB_USERNAME, "root");
+define(DB_PASSWORD, "");
+
+//$db_hostname = "localhost";
+//$db_database = "Nest";
+//$db_username = "root";
+//$db_password = "";
+//$appName = "Jerry's Nest";
+$db_server = mysqli_connect(DB_HOSTNAME, DB_USENAME, DB_USERNAME);
 if (!$db_server) die("Unable to connect to MYSQL".mysqli_error($db_server));
-mysqli_select_db($db_server, $db_database) or die("Unable to select database".mysqli_error($db_server));
+mysqli_select_db($db_server, DB_DATABASE) or die("Unable to select database".mysqli_error($db_server));
 
 //创建数据库表
 function createTable($name, $query)
@@ -54,7 +60,7 @@ function showProfile($user)
     if (file_exists("user.jpg"))    //检查文件或者目录是否存在。括号内文件名或者路径
         echo "<img src = 'user.jpg' align = 'left'/>";  //插入图片
 
-    $result = queryMysql("SELECT * FROM profiles WHERE user = '$user'");
+    $result = queryMysql("SELECT * FROM profiles WHERE user = '$user'");    //查询可以采用sprintf函数接收全部变量的值赋给%s
     if (mysqli_num_rows($result))
     {
         $row = mysqli_fetch_row($result);
