f6af8647c347ab53e4a56afa3c5f529c640c990d
Jerry <938334866@qq.com>
2016-08-20 16:39:27 +0800

    header文件建立首页

--- header.php
+++ header.php
@@ -6,4 +6,53 @@
  * Time: 20:26
  */
 
+echo "<!DOCTYPE html>
+<html>
+    <head>
+        <script src='OSC.js'></script>";
+require_once "functions.php";
+session_start();
 
+$userstr = ' (Guest)';
+
+
+if (isset($_SESSION['user']))
+{
+    $user = $_SESSION['user'];
+    $loggedin = TRUE;
+    $userstr = "($user)";
+}
+else
+{
+    echo $loggedin = FALSE;
+}
+//  链接外部样式表
+echo "<title>$appname$userstr</title>.
+      <link rel='stylesheet' href='style.css' type='text/css'>.  
+      </head>
+      <body>
+      <canvas id='logo' width='624' height='96'>$appname</canvas>
+      <div class='appname'>$appname$userstr</div></body>
+      <script src='javascript.js'></script>";
+
+if ($loggedin)
+{
+    echo "<br><ul class='menu'>.
+          <li><a href='member.php?view=$user'></a></li>
+          <li><a href='member.php'>Members</a> </li>
+          <li><a href='frends.php'>Frends</a> </li>
+          <li><a href='messages.php'>Messages</a></li>
+          <li><a href='profile.php'>Edit Profile</a></li>
+          <li><a href='logout.php'>Log out</a> </li>
+    </ul></br>";
+}
+else
+{
+    echo"<br><ul class='menu'>
+        <li><a href='index.php'>Home</a></li>             
+        <li><a href='signup.php'>Sign up</a></li>
+        <li><a href='login.php'>Log in</a></li></ul><br>
+        <span class='info'>&#8658; You must be logged in to
+        view this page.</span><br><br>";
+}
+?>
